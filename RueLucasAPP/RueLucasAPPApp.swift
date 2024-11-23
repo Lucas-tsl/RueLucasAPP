7118f6c3d1581d8c70925ba9e617761a0561946a
Lucas Troteseil <troteseil.lucas@gmail.com>
2024-11-23 11:01:44 +0100

    Initial Commit

---
+++ RueLucasAPP/RueLucasAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  RueLucasAPPApp.swift
+//  RueLucasAPP
+//
+//  Created by Lucas Pro on 23/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct RueLucasAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RueLucasAPPTests/RueLucasAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  RueLucasAPPTests.swift
+//  RueLucasAPPTests
+//
+//  Created by Lucas Pro on 23/11/2024.
+//
+
+import Testing
+@testable import RueLucasAPP
+
+struct RueLucasAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
